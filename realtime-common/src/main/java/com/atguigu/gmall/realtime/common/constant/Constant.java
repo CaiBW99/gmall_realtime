480d7a39d28257aa8b8e559a45a0abfe87eebd57
天贶十三 <atguigu@atguigu.com>
2024-01-27 09:48:32 +0800

    Changes

---
+++ realtime-common/src/main/java/com/atguigu/gmall/realtime/common/constant/Constant.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.realtime.common.constant;
+
+/**
+ * @ClassName Constant
+ * @Package com.atguigu.gmall.realtime.common.constant
+ * @Author CaiBW
+ * @Create 24/01/27 上午 12:01
+ * @Description
+ */
+public class Constant {
+}
