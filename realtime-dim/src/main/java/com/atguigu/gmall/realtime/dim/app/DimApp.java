79100660d5a0c6a5462457b4bb8b81a921d83ace
天贶十三 <atguigu@atguigu.com>
2024-01-30 15:53:11 +0800

    changes

--- realtime-dim/src/main/java/com/atguigu/gmall/realtime/dim/app/DimApp.java
+++ realtime-dim/src/main/java/com/atguigu/gmall/realtime/dim/app/DimApp.java
@@ -357,7 +357,6 @@ public class DimApp extends BaseApp {
                     }
                 }
         );
-        System.out.println("=============================================================");
     }
 
     /**
