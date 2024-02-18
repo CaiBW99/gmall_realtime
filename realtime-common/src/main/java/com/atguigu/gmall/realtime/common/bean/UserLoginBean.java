4d5a94942d359354f53758d84ea9ef3ccec5ec26
天贶十三 <atguigu@atguigu.com>
2024-02-18 18:21:58 +0800

    changes

--- realtime-common/src/main/java/com/atguigu/gmall/realtime/common/bean/UserLoginBean.java
+++ realtime-common/src/main/java/com/atguigu/gmall/realtime/common/bean/UserLoginBean.java
@@ -2,12 +2,14 @@ package com.atguigu.gmall.realtime.common.bean;
 
 import com.alibaba.fastjson.annotation.JSONField;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserLoginBean {
     // 窗口起始时间
     String stt;
