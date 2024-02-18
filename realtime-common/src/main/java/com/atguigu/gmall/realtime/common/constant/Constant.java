d4bb5c7f78c42a159109700777902ae2a1857ef8
天贶十三 <atguigu@atguigu.com>
2024-02-18 11:53:15 +0800

    changes

--- realtime-common/src/main/java/com/atguigu/gmall/realtime/common/constant/Constant.java
+++ realtime-common/src/main/java/com/atguigu/gmall/realtime/common/constant/Constant.java
@@ -28,11 +28,19 @@ public class Constant {
     
     
     public static final String HBASE_NAMESPACE = "gmall_realtime";
-
     public static final String HBASE_ZOOKEEPER_QUORUM = "linux1,linux2,linux3";
-
-
-
+    
+    public static final String DORIS_FENODES = "linux1:7030";
+    public static final String DORIS_DATABASE = "gmall_realtime";
+    
+    public static final String DORIS_USERNAME = "root";
+    public static final String DORIS_PASSWORD = "000000";
+    
+    
+    public static final String DWS_TRAFFIC_SOURCE_KEYWORD_PAGE_VIEW_WINDOW = "dws_traffic_source_keyword_page_view_window";
+    public static final String DWS_TRAFFIC_VC_CH_AR_IS_NEW_PAGE_VIEW_WINDOW = "dws_traffic_vc_ch_ar_is_new_page_view_window";
+    public static final String DWS_TRAFFIC_HOME_DETAIL_PAGE_VIEW_WINDOW = "dws_traffic_home_detail_page_view_window";
+    
     public static final String TOPIC_DWD_TRAFFIC_START = "dwd_traffic_start";
     public static final String TOPIC_DWD_TRAFFIC_ERR = "dwd_traffic_err";
     public static final String TOPIC_DWD_TRAFFIC_PAGE = "dwd_traffic_page";
@@ -40,16 +48,12 @@ public class Constant {
     public static final String TOPIC_DWD_TRAFFIC_DISPLAY = "dwd_traffic_display";
 
     public static final String TOPIC_DWD_INTERACTION_COMMENT_INFO = "dwd_interaction_comment_info";
+    
     public static final String TOPIC_DWD_TRADE_CART_ADD = "dwd_trade_cart_add";
-
     public static final String TOPIC_DWD_TRADE_ORDER_DETAIL = "dwd_trade_order_detail";
-
     public static final String TOPIC_DWD_TRADE_ORDER_CANCEL = "dwd_trade_order_cancel";
-
     public static final String TOPIC_DWD_TRADE_ORDER_PAYMENT_SUCCESS = "dwd_trade_order_payment_success";
     public static final String TOPIC_DWD_TRADE_ORDER_REFUND = "dwd_trade_order_refund";
-
     public static final String TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS = "dwd_trade_refund_payment_success";
-
     public static final String TOPIC_DWD_USER_REGISTER = "dwd_user_register";
 }
