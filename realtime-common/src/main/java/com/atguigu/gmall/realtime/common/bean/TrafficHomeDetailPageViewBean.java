7c4c9acc1510a9c6416d8fd807a8a3b4d2f10e17
天贶十三 <atguigu@atguigu.com>
2024-02-18 11:59:53 +0800

    changes

--- realtime-common/src/main/java/com/atguigu/gmall/realtime/common/bean/TrafficHomeDetailPageViewBean.java
+++ realtime-common/src/main/java/com/atguigu/gmall/realtime/common/bean/TrafficHomeDetailPageViewBean.java
@@ -1,5 +1,10 @@
 package com.atguigu.gmall.realtime.common.bean;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
 /**
  * @ClassName TrafficHomeDetailPageViewBean
  * @Package com.atguigu.gmall.realtime.common.bean
@@ -7,5 +12,21 @@ package com.atguigu.gmall.realtime.common.bean;
  * @Create 24/02/18 上午 11:49
  * @Description
  */
+@Data
+@AllArgsConstructor
+@Builder
 public class TrafficHomeDetailPageViewBean {
+    // 窗口起始时间
+    String stt;
+    // 窗口结束时间
+    String edt;
+    // 当天日期
+    String curDate;
+    // 首页独立访客数
+    Long homeUvCt;
+    // 商品详情页独立访客数
+    Long goodDetailUvCt;
+    // 时间戳
+    @JSONField(serialize = false)
+    Long ts;
 }
