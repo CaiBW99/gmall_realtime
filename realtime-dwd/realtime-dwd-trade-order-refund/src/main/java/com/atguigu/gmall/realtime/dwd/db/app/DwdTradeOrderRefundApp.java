518c56b4006be178726308ea0cdcb00f77c7406f
天贶十三 <atguigu@atguigu.com>
2024-02-22 16:52:38 +0800

    changes

--- realtime-dwd/realtime-dwd-trade-order-refund/src/main/java/com/atguigu/gmall/realtime/dwd/db/app/DwdTradeOrderRefundApp.java
+++ realtime-dwd/realtime-dwd-trade-order-refund/src/main/java/com/atguigu/gmall/realtime/dwd/db/app/DwdTradeOrderRefundApp.java
@@ -1,5 +1,12 @@
 package com.atguigu.gmall.realtime.dwd.db.app;
 
+import com.atguigu.gmall.realtime.common.base.BaseSQLApp;
+import com.atguigu.gmall.realtime.common.constant.Constant;
+import com.atguigu.gmall.realtime.common.util.FlinkSQLUtil;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
 /**
  * @ClassName DwdTradeOrderRefundApp
  * @Package com.atguigu.gmall.realtime.dwd.db.app
@@ -7,5 +14,102 @@ package com.atguigu.gmall.realtime.dwd.db.app;
  * @Create 24/02/18 上午 11:37
  * @Description 交易域退单事务事实表 (练习)
  */
-public class DwdTradeOrderRefundApp {
+public class DwdTradeOrderRefundApp extends BaseSQLApp {
+    public static void main(String[] args) {
+        new DwdTradeOrderRefundApp().start(
+            10017,
+            4,
+            Constant.TOPIC_DWD_TRADE_ORDER_REFUND);
+    }
+    
+    @Override
+    public void handle(StreamTableEnvironment tableEnv, StreamExecutionEnvironment env, String groupId) {
+        //TODO 1.读取topic_db数据, 读取字典表
+        readOdsTopicDb(tableEnv, Constant.TOPIC_DWD_TRADE_ORDER_REFUND);
+        readBaseDic(tableEnv);
+        
+        //TODO 2.过滤退单表数据 order_refund_info insert
+        Table orderRefundInfo = tableEnv.sqlQuery(
+            "SELECT data['id']                 id,\n" +
+                "       data['user_id']            user_id,\n" +
+                "       data['order_id']           order_id,\n" +
+                "       data['sku_id']             sku_id,\n" +
+                "       data['refund_type']        refund_type,\n" +
+                "       data['refund_num']         refund_num,\n" +
+                "       data['refund_amount']      refund_amount,\n" +
+                "       data['refund_reason_type'] refund_reason_type,\n" +
+                "       data['refund_reason_txt']  refund_reason_txt,\n" +
+                "       data['create_time']        create_time,\n" +
+                "       pt,\n" +
+                "       ts\n" +
+                "FROM topic_db\n" +
+                "WHERE `database` = 'gmall'\n" +
+                "  AND `table` = 'order_refund_info'\n" +
+                "  AND `type` = 'insert'"
+        );
+        tableEnv.createTemporaryView("order_refund_info", orderRefundInfo);
+        
+        //TODO 3.过滤订单表中的退单数据: order_info  update
+        Table orderInfo = tableEnv.sqlQuery(
+            "select \n" +
+                "data['id'] id,\n" +
+                "data['province_id'] province_id,\n" +
+                "`old` \n" +
+                "from topic_db \n" +
+                "where `database`='gmall' \n" +
+                "and `table`='order_info' \n" +
+                "and `type`='update'\n" +
+                "and `old`['order_status'] is not null \n" +
+                "and `data`['order_status']='1005'"
+        );
+        tableEnv.createTemporaryView("order_info", orderInfo);
+        
+        //TODO 4.join
+        Table result = tableEnv.sqlQuery(
+            "SELECT ri.id,\n" +
+                "       ri.user_id,\n" +
+                "       ri.order_id,\n" +
+                "       ri.sku_id,\n" +
+                "       oi.province_id,\n" +
+                "       DATE_FORMAT( ri.create_time, 'yyyy-MM-dd' ) date_id,\n" +
+                "       ri.create_time,\n" +
+                "       ri.refund_type,\n" +
+                "       dic1.info.dic_name,\n" +
+                "       ri.refund_reason_type,\n" +
+                "       dic2.info.dic_name,\n" +
+                "       ri.refund_reason_txt,\n" +
+                "       ri.refund_num,\n" +
+                "       ri.refund_amount,\n" +
+                "       ri.ts\n" +
+                "FROM order_refund_info ri\n" +
+                "    JOIN order_info oi\n" +
+                "         ON ri.order_id = oi.id\n" +
+                "    JOIN base_dic FOR SYSTEM_TIME AS OF ri.pt AS dic1\n" +
+                "         ON ri.refund_type=dic1.dic_code\n" +
+                "    JOIN base_dic FOR SYSTEM_TIME AS OF ri.pt AS dic2\n" +
+                "         ON ri.refund_reason_type=dic2.dic_code"
+        );
+        
+        //TODO 5.写出到 kafka
+        tableEnv.executeSql(
+            "CREATE TABLE dwd_trade_order_refund(\n" +
+                "    id STRING,\n" +
+                "    user_id STRING,\n" +
+                "    order_id STRING,\n" +
+                "    sku_id STRING,\n" +
+                "    province_id STRING,\n" +
+                "    date_id STRING,\n" +
+                "    create_time STRING,\n" +
+                "    refund_type_code STRING,\n" +
+                "    refund_type_name STRING,\n" +
+                "    refund_reason_type_code STRING,\n" +
+                "    refund_reason_type_name STRING,\n" +
+                "    refund_reason_txt STRING,\n" +
+                "    refund_num STRING,\n" +
+                "    refund_amount STRING,\n" +
+                "    ts BIGINT\n" +
+                ")" + FlinkSQLUtil.getKafkaSinkDDL(Constant.TOPIC_DWD_TRADE_ORDER_REFUND)
+        );
+        result.executeInsert(Constant.TOPIC_DWD_TRADE_ORDER_REFUND);
+    }
 }
--- realtime-dwd/realtime-dwd-trade-refund-pay-suc-detail/src/main/java/com/atguigu/gmall/realtime/dwd/db/app/DwdTradeRefundPaySucDetailApp.java
+++ realtime-dwd/realtime-dwd-trade-refund-pay-suc-detail/src/main/java/com/atguigu/gmall/realtime/dwd/db/app/DwdTradeRefundPaySucDetailApp.java
@@ -1,5 +1,12 @@
 package com.atguigu.gmall.realtime.dwd.db.app;
 
+import com.atguigu.gmall.realtime.common.base.BaseSQLApp;
+import com.atguigu.gmall.realtime.common.constant.Constant;
+import com.atguigu.gmall.realtime.common.util.FlinkSQLUtil;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
 /**
  * @ClassName DwdTradeRefundPaySucDetailApp
  * @Package com.atguigu.gmall.realtime.dwd.db.app
@@ -7,5 +14,104 @@ package com.atguigu.gmall.realtime.dwd.db.app;
  * @Create 24/02/18 上午 11:38
  * @Description 交易域退款成功事务事实表（练习）
  */
-public class DwdTradeRefundPaySucDetailApp {
+public class DwdTradeRefundPaySucDetailApp extends BaseSQLApp {
+    public static void main(String[] args) {
+        new DwdTradeRefundPaySucDetailApp().start(10018, 4, Constant.TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS);
+    }
+    
+    @Override
+    public void handle(StreamTableEnvironment tableEnv, StreamExecutionEnvironment env, String groupId) {
+        //TODO 1.读取topic_db,读取字典表
+        readOdsTopicDb(tableEnv, Constant.TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS);
+        readBaseDic(tableEnv);
+        
+        //TODO 2.过滤退款成功表数据
+        Table refundPayment = tableEnv.sqlQuery(
+            "SELECT data['id']            id,\n" +
+                "       data['order_id']      order_id,\n" +
+                "       data['sku_id']        sku_id,\n" +
+                "       data['payment_type']  payment_type,\n" +
+                "       data['callback_time'] callback_time,\n" +
+                "       data['total_amount']  total_amount,\n" +
+                "       pt,\n" +
+                "       ts\n" +
+                "FROM topic_db\n" +
+                "WHERE `database` = 'gmall'\n" +
+                "  AND `table` = 'refund_payment'\n" +
+                "  AND `type` = 'update'\n" +
+                "  AND `old`['refund_status'] IS NOT NULL\n" +
+                "  AND `data`['refund_status'] = '1602'"
+        );
+        tableEnv.createTemporaryView("refund_payment", refundPayment);
+        
+        //TODO 3.过滤退单表中的退单成功数据
+        Table orderRefundInfo = tableEnv.sqlQuery(
+            "SELECT data['order_id']   order_id,\n" +
+                "       data['sku_id']     sku_id,\n" +
+                "       data['refund_num'] refund_num\n" +
+                "FROM topic_db\n" +
+                "WHERE `database` = 'gmall'\n" +
+                "  AND `table` = 'order_refund_info'\n" +
+                "  AND `type` = 'update'\n" +
+                "  AND `old`['refund_status'] IS NOT NULL\n" +
+                "  AND `data`['refund_status'] = '0705'"
+        );
+        tableEnv.createTemporaryView("order_refund_info", orderRefundInfo);
+        
+        //TODO 4.过滤订单表中的退款成功数据
+        Table orderInfo = tableEnv.sqlQuery(
+            "SELECT data['id']          id,\n" +
+                "       data['user_id']     user_id,\n" +
+                "       data['province_id'] province_id\n" +
+                "FROM topic_db\n" +
+                "WHERE `database` = 'gmall'\n" +
+                "  AND `table` = 'order_info'\n" +
+                "  AND `type` = 'update'\n" +
+                "  AND `old`['order_status'] IS NOT NULL\n" +
+                "  AND `data`['order_status'] = '1006'"
+        );
+        tableEnv.createTemporaryView("order_info", orderInfo);
+        
+        //TODO 5.join
+        Table result = tableEnv.sqlQuery(
+            "SELECT rp.id,\n" +
+                "       oi.user_id,\n" +
+                "       rp.order_id,\n" +
+                "       rp.sku_id,\n" +
+                "       oi.province_id,\n" +
+                "       rp.payment_type,\n" +
+                "       dic.info.dic_name                             payment_type_name,\n" +
+                "       DATE_FORMAT( rp.callback_time, 'yyyy-MM-dd' ) date_id,\n" +
+                "       rp.callback_time,\n" +
+                "       ori.refund_num,\n" +
+                "       rp.total_amount,\n" +
+                "       rp.ts\n" +
+                "FROM refund_payment rp\n" +
+                "    JOIN order_refund_info ori\n" +
+                "         ON rp.order_id = ori.order_id AND rp.sku_id = ori.sku_id\n" +
+                "    JOIN order_info oi\n" +
+                "         ON rp.order_id = oi.id\n" +
+                "    JOIN base_dic FOR SYSTEM_TIME AS OF rp.pt AS dic\n" +
+                "ON rp.payment_type=dic.dic_code"
+        );
+        
+        //TODO 6.写入到Kafka
+        tableEnv.executeSql(
+            "CREATE TABLE " + Constant.TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS + "(\n" +
+                "    id STRING,\n" +
+                "    user_id STRING,\n" +
+                "    order_id STRING,\n" +
+                "    sku_id STRING,\n" +
+                "    province_id STRING,\n" +
+                "    payment_type_code STRING,\n" +
+                "    payment_type_name STRING,\n" +
+                "    date_id STRING,\n" +
+                "    callback_time STRING,\n" +
+                "    refund_num STRING,\n" +
+                "    refund_amount STRING,\n" +
+                "    ts BIGINT\n" +
+                ")" + FlinkSQLUtil.getKafkaSinkDDL(Constant.TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS)
+        );
+        result.insertInto(Constant.TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS).execute();
+    }
 }
