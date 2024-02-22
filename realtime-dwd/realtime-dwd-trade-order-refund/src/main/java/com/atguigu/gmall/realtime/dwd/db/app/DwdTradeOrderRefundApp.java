2209627d208340ba593bd232f738663c6edfb9a2
天贶十三 <atguigu@atguigu.com>
2024-02-22 13:55:17 +0800

    changes

---
+++ realtime-dwd/realtime-dwd-trade-order-refund/src/main/java/com/atguigu/gmall/realtime/dwd/db/app/DwdTradeOrderRefundApp.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.realtime.dwd.db.app;
+
+/**
+ * @ClassName DwdTradeOrderRefundApp
+ * @Package com.atguigu.gmall.realtime.dwd.db.app
+ * @Author CaiBW
+ * @Create 24/02/18 上午 11:37
+ * @Description 交易域退单事务事实表 (练习)
+ */
+public class DwdTradeOrderRefundApp {
+}
---
+++ realtime-dwd/realtime-dwd-trade-refund-pay-suc-detail/src/main/java/com/atguigu/gmall/realtime/dwd/db/app/DwdTradeRefundPaySucDetailApp.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.realtime.dwd.db.app;
+
+/**
+ * @ClassName DwdTradeRefundPaySucDetailApp
+ * @Package com.atguigu.gmall.realtime.dwd.db.app
+ * @Author CaiBW
+ * @Create 24/02/18 上午 11:38
+ * @Description 交易域退款成功事务事实表（练习）
+ */
+public class DwdTradeRefundPaySucDetailApp {
+}
